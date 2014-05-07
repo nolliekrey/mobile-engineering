e277dc2338941337775213adc90bc54b3d84c671
Jon Whitmore <nollie@gmail.com>
2014-05-06 22:38:14 -0400

    Added nag controller
    Added a navigation controller

---
+++ ExistingCommunal/ExistingCommunal/ECNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  ECNavigationController.h
+//  ExistingCommunal
+//
+//  Created by Jon Whitmore on 5/6/14.
+//  Copyright (c) 2014 Jon Whitmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECNavigationController : UINavigationController
+
+@end
---
+++ ExistingCommunal/ExistingCommunal/ECNavigationController.m
@@ -0,0 +1,42 @@
+//
+//  ECNavigationController.m
+//  ExistingCommunal
+//
+//  Created by Jon Whitmore on 5/6/14.
+//  Copyright (c) 2014 Jon Whitmore. All rights reserved.
+//
+
+#import "ECNavigationController.h"
+
+@interface ECNavigationController ()
+
+@end
+
+@implementation ECNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+//    [self.navigationController setNavigationBarHidden:NO
+//                                             animated:YES];
+//    [[UIApplication sharedApplication] setStatusBarHidden:NO
+//                                            withAnimation:UIStatusBarAnimationFade];
+//	self.navigationItem.title = @"Your offers";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
